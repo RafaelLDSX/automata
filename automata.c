c1476c56a2a1613dc522f0bfe2c4e52e3b55fc7c
Rafael Xavier <rafael_ldsx@hotmail.com>
2018-09-10 09:08:46 -0300

    Added LIST to manage multiple entities

--- automata.c
+++ automata.c
@@ -7,18 +7,23 @@
 typedef struct point {
 	int x;
 	int y;
+	int id;
+	int alive;
 } POINT;
 
 typedef struct list {
 	POINT point;
-	int id;
 	struct list* next;
 } LIST;
 
+void addLIST(LIST* list, LIST* new);
+void addPOINTtoLIST(LIST* list, POINT* p);
+void removefromLIST(LIST* list, int id);
+LIST* initLIST(void);
 POINT* initPOINT(void);
 int** initMatrix(void);
 bool checkMatrix(int** matrix, int x, int y );
-void move(int** matrix, int** temp, POINT* p, POINT* door );
+void move(int** matrix, int** temp, LIST* list, POINT* door );
 void freeMatrix(int** matrix);
 
 static int sairam = 0;
@@ -32,12 +37,26 @@ int main(void)
 	matrix[7][5] = 1; 	
 	matrix[0][1] = 2;
 
+	LIST* list = initLIST();
+
 	POINT* door = initPOINT();
 	door->x = 0;
 	door->y = 1;
+	door->id = 2;
+
 	POINT* aux = initPOINT();
 	aux->x = 7;
 	aux->y = 5;
+	aux->id = 1;
+	aux->alive = 1;
+
+	addPOINTtoLIST(list, aux);
+
+	aux->x = 4;
+	aux->y = 6;
+	aux->id = 4;
+
+	addPOINTtoLIST(list, aux);
 
 	int counter = 0; 	
 
@@ -59,7 +78,7 @@ int main(void)
 		// 		if(matrix[i][j] == 1){
 		// 			aux->x = i;
 		// 			aux->y = j;
-					move(matrix, temp, aux, door);
+					move(matrix, temp, list, door);
 		// 		}
 		// 	}
 		// }
@@ -71,6 +90,8 @@ int main(void)
 		}
 		printf("\n");
 		counter++;
+		printf("Sairam: %d\n\n", sairam);
+		sleep(1);
 	}
 	free(door);
 	free(aux);
@@ -79,51 +100,56 @@ int main(void)
 }
 
 bool checkMatrix(int** matrix, int x, int y){
-	if ( matrix[x][y] != 0 ){
+	if ( matrix[x][y] != 0 && matrix[x][y] != 2 ){
 		return false;
 	}
 	return true;
 }
 
-void move(int** matrix, int** temp, POINT* p, POINT* door )
+void move(int** matrix, int** temp, LIST* list, POINT* door )
 {
+	LIST* aux = list;
+	while(aux != NULL){
+		if(aux->point.alive == 1){
+			POINT* novo = initPOINT();
 
-	POINT* novo = initPOINT();
+			novo->x = aux->point.x;
+			novo->y = aux->point.y;
+			novo->id = aux->point.id;
 
-	novo->x = p->x;
-	novo->y = p->y;
+			if(aux->point.x < door->x && checkMatrix(matrix, aux->point.x+1, aux->point.y)){
+				novo->x = aux->point.x + 1;
+			}
+			if(aux->point.y < door->y && checkMatrix(matrix, aux->point.x, aux->point.y+1)){
+				novo->y = aux->point.y + 1;
+			}
+			if(aux->point.x > door->x && checkMatrix(matrix, aux->point.x-1, aux->point.y)){
+				novo->x = aux->point.x - 1;
+			}
+			if(aux->point.y > door->y && checkMatrix(matrix, aux->point.x, aux->point.y-1)){
+				novo->y = aux->point.y - 1;
+			}
 
-	POINT right = { .x = p->x+1, .y = p->y};
-	POINT left = { .x = p->x-1, .y = p->y};
-	POINT down = { .x = p->x, .y = p->y+1};
-	POINT up = { .x = p->x, .y = p->y-1};
-	if(p->x < door->x && checkMatrix(matrix, p->x+1, p->y)){
-		novo->x = p->x + 1;
+			matrix[aux->point.x][aux->point.y] = 0;
+			if(matrix[novo->x][novo->y] == 2){
+				sairam++;
+				aux->point.alive = 0;
+				removefromLIST(aux, novo->id);
+			}
+			else{
+				matrix[novo->x][novo->y] = 1;
+			}
+
+			temp[novo->x][novo->y] = 1;
+
+			aux->point.x = novo->x;
+			aux->point.y = novo->y;
+
+			free(novo);
+
+		}
+		aux = aux->next;
 	}
-	if(p->y < door->y && checkMatrix(matrix, p->x, p->y+1)){
-		novo->y = p->y + 1;
-	}
-	if(p->x > door->x && checkMatrix(matrix, p->x-1, p->y)){
-		novo->x = p->x - 1;
-	}
-	if(p->y > door->y && checkMatrix(matrix, p->x, p->y-1)){
-		novo->y = p->y - 1;
-	}
-
-	matrix[p->x][p->y] = 0;
-	if(matrix[novo->x][novo->y] == 2){
-		sairam++;
-	}
-	else{
-		matrix[novo->x][novo->y] = 1;
-	}
-
-	temp[novo->x][novo->y] = 1;
-
-	p->x = novo->x;
-	p->y = novo->y;
-
-	free(novo);
 }
 
 int** initMatrix(void) {
@@ -139,6 +165,50 @@ int** initMatrix(void) {
 		}
 	}
 	return matrix;
+}
+
+LIST* initLIST(void){
+	LIST* list = malloc(sizeof(LIST));
+	list->next = NULL;
+	return list;
+}
+
+void addLIST(LIST* list, LIST* new){
+	LIST* temp = list;
+	while(temp->next != NULL){
+		temp = temp->next;
+	}
+	temp->next = new;
+}
+
+void addPOINTtoLIST(LIST* list, POINT* p){
+	LIST* aux = initLIST();
+	aux->point.x = p->x;
+	aux->point.y = p->y;
+	aux->point.id = p->id;
+	aux->point.alive = p->alive;
+	addLIST(list, aux);
+}
+
+void removefromLIST(LIST* list, int id){
+	LIST* aux = NULL;
+	LIST* temp = list;
+	while(temp->next != NULL && temp->point.id != id){
+		aux = temp;
+		temp = temp->next;
+	}
+
+	if(temp->point.id == id){
+		if(aux == NULL){
+			list = temp->next;
+			free(temp);
+		}
+		else{
+			aux->next = temp->next;
+			free(temp);
+		}
+	}
+
 }
 
 POINT* initPOINT(void) {
